0d7dfe716657e3c48be19212d95096f871fff37c
Kwaku J <gyazi@live.com>
2018-04-14 22:52:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,12 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+let index = 0
+
 function init() {
   // Write your JavaScript code inside the init() function
   var bod = document.querySelector('body')
   bod.addEventListener('keydown', function(e){
-    for(var i = 0; i < code.length; i++){
-      if (e.which === code[i]){
+    var key = parseInt(e.detail || e.which)
+    
+    if (key === code[index]){
+      index++;
+      
+      if (index === code.length){
         alert("Congratulation")
+        index = 0
+      }
+      else{
+        index = 0
       }
     }
  })
